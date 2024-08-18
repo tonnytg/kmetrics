4680dca2ea8f2f49ab0c2f12188e7e82442d6dfb
Antonio Thomacelli <tonnytg@gmail.com>
2024-08-17 23:43:12 -0300

    feat: initial commit with tdd

---
+++ pkg/metrics/metrics_test.go
@@ -0,0 +1,15 @@
+package metrics_test
+
+import "testing"
+
+func TestFetchMetrics(t *testing.T) {
+    t.Run("should return metrics data from Kubernetes API", func(t *testing.T) {
+        got := FetchMetrics()
+        want := "expected_metrics_data"
+
+        if got != want {
+            t.Errorf("got %q, want %q", got, want)
+        }
+    })
+}
+
